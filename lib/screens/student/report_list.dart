6f280dfe7fe396ad21f66c469f89c7ebf91db22d
Youssef Metwalli <ymetwalli@Youssefs-MacBook-Pro.local>
2023-11-23 21:58:58 +0900

    ta screens were added

---
+++ lib/screens/student/report_list.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class StudentScreen extends StatefulWidget {
+  @override
+  _StudentScreenState createState() => _StudentScreenState();
+}
+
+class _StudentScreenState extends State<StudentScreen> {
+  // Sample list for the body
+  List<String> itemList = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  // Sample list of notifications
+  List<String> notifications = [
+    'Notification 1',
+    'Notification 2',
+    'Notification 3',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              // Handle logout button press
+              // Add your logout logic here
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(28.0), // Add padding on all sides
+        child: ListView.separated(
+          itemCount: itemList.length,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 8.0); // Adjust the height of the separator
+          },
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black, // Border color
+                  width: 1.0, // Border width
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(8.0), // Border radius
+                ),
+              ),
+              child: ListTile(
+                title: Text(itemList[index]),
+                onTap: () {
+                  // Handle the tap on an item
+                  print('Tapped on: ${itemList[index]}');
+                  // Add your item handling logic here
+                },
+              ),
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Notifications',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            for (var notification in notifications)
+              ListTile(
+                title: Text(notification),
+                onTap: () {
+                  // Handle the tap on a notification
+                  print('Tapped on: $notification');
+                  // Add your notification handling logic here
+                },
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
