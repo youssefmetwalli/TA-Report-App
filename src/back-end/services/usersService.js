f1b1abe9695b12972f797b37412b794251325218
Merci <habmercima@gmail.com>
2023-11-26 23:36:39 +0900

    graphql initial

---
+++ src/back-end/services/usersService.js
@@ -0,0 +1,12 @@
+// services/usersService.js
+const UsersDto = require('../dto/usersDto');
+
+class UsersManagerService {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  // Implement methods for CRUD operations
+}
+
+module.exports = UsersManagerService;
